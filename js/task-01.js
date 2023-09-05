978eac94b7cd4a3dfe7e19c88b9bef567cdcc4b7
FunnyBilbur <tonistark1997@gmail.com>
2023-09-05 22:06:43 +0300

    +

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+"use strict";
+
+
+const categories_counter = document.querySelector("#categories");
+console.dir(categories_counter);
+console.log("Total is: ", categories_counter.children.length);
